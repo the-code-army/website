e27e3f9764adc0d075197f9348ca860fe063eb50
zambony <3318117+zambony@users.noreply.github.com>
2020-04-19 00:15:33 +0000

    Remove extra fetch request for avatar
    Also made generic function to retrieve avatar

--- pages/api/avatar.js
+++ pages/api/avatar.js
@@ -1,30 +1,54 @@
 import fetch from 'node-fetch';
-module.exports = async (req, res) => {
-  const { id } = req.query;
-  if(!id) {
-    res.status(400).send({
-      ok:false,
-      message: 'No ID'
-    })
-    return;
-  }
-  let result = await fetch(`https://discordapp.com/api/users/${id}`, {
-    headers: {
-      'Authorization': `Bot ${process.env.TOKEN}`
-    }
-  })
-  if(!result.ok) {
-    res.status(result.status)
-    return;
-  }
-  let json = await result.json();
-  // console.log(json)
-  let avatar = await fetch(`https://cdn.discordapp.com/avatars/${id}/${json.avatar}.gif`);
-  if(avatar.ok == false) {
-      avatar = await fetch(
-        `https://cdn.discordapp.com/avatars/${id}/${json.avatar}.png`
-      );
-  }
-  let buffer = await avatar.buffer();
-  res.status(200).send(buffer)
+
+export default async function(req, res) {
+	const {id} = req.query
+
+	try
+	{
+		const buffer = await getAvatar(id)
+
+		res.status(200).send(buffer)
+	}
+	catch (error)
+	{
+		switch (error.constructor)
+		{
+			case TypeError:
+				res.status(400).send({
+					ok: false,
+					message: "No ID"
+				})
+				break
+			default:
+				res.status(error.code)
+				break
+		}
+	}
+}
+
+async function getAvatar(id)
+{
+	if (!id)
+	{
+		throw new TypeError(`"id" is not a non-null object`)
+	}
+
+	let result = await fetch(`https://discordapp.com/api/users/${id}`, {
+		headers: {
+			'Authorization': `Bot ${process.env.TOKEN}`
+		}
+	})
+
+	if (!result.ok)
+	{
+		const err = new Error("Discord API failure")
+		err.code = result.status
+
+		throw err
+	}
+
+	const json = await result.json()
+	const avatar = await fetch(`https://cdn.discordapp.com/avatars/${id}/${json.avatar}`)  // no need for extension
+
+	return await avatar.buffer()
 }
